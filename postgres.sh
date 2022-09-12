5a92834855c169f9e34fc4ff4216ab3f61e34358
Sreenath Reddy <110103544+Sreenath732@users.noreply.github.com>
2022-09-12 11:35:33 +0530

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -1,4 +1,5 @@
-cd project/configuration_script
-sudo chmod 777 *
-cd project/configuration_script
-./configuration.sh
+pwd
+#cd project/configuration_script
+#sudo chmod 777 *
+#cd project/configuration_script
+#./configuration.sh
